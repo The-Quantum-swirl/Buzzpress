96b102df2e547d3838c84e2872dd4970c583be90
Mukhar Jain <Mukhar@users.noreply.github.com>
2021-09-19 19:19:39 +0530

    Render bug fi
    Bug Fix for rendering on adding component

--- ui/src/screens/Create.jsx
+++ ui/src/screens/Create.jsx
@@ -1,89 +1,91 @@
 import NavBar from "../components/NavBar";
 import { useState } from "react";
 import { Steps, Row, Col } from 'antd';
-import {  Button, Input, Select, Space, Divider } from 'antd';
-import {TextField} from '@mui/material';
+import { Button, Input, Select, Space, Divider } from 'antd';
+import { TextField } from '@mui/material';
 const { TextArea } = Input;
 const { Step } = Steps;
 
-export default function Create (){
-    const [heading, setHeading]= useState("");
-    const [subHeading, setSubHeading]= useState("");
-    const [para, setPara] = useState([""]);
+export default function Create() {
+  const [heading, setHeading] = useState("");
+  const [subHeading, setSubHeading] = useState("");
+  let [para, setPara] = useState([""]);
 
-    function handleChange (event, position) {
-        let oldPara = para;
-        oldPara[position] = event.target.value;
-        setPara( oldPara );
-    }
+  function handleChange(event, position) {
+    let oldPara = para;
+    oldPara[position] = event.target.value;
+    setPara(oldPara);
+  }
 
-    function handleFieldChange(event, pos){
-        let oldPara = para;
-        oldPara.splice(pos+1, 0, "");
-        setPara( oldPara );
-    }
+  function handleFieldChange(event, pos) {
+    console.log(pos);
+    let oldPara = [...para];
+    oldPara.splice(pos + 1, 0, "");
+    console.log(oldPara);
+    setPara(oldPara);
+  }
 
-    return(
+  return (
     <>
-    <NavBar />
-    <Row>
+      <NavBar />
+      <Row>
 
-    <Col offset={6} span={12}>
-    {/* <Space direction="vertical"> */}
-    
-        <Steps size="default" current={0} style={{marginTop:'3%',padding:'2%'}}>
+        <Col offset={6} span={12}>
+          {/* <Space direction="vertical"> */}
+
+          <Steps size="default" current={0} style={{ marginTop: '3%', padding: '2%' }}>
             <Step title="Create" />
             <Step title="Preview" />
             <Step title="Share" />
-        </Steps>
-        
-        <Divider style={{margin:'1% 0 5% 0', width:'100%', minWidth:'30%'}} />
+          </Steps>
 
-        <TextField
-          id="standard-textarea"
-        //   label=
-          placeholder="Heading"
-          multiline
-          variant="standard"
-        //   value={draft[0]} 
-          onChange={(e) => setHeading(e.target.value)}
-          style={{width:'100%',marginBottom: '28px' }}
-        />
+          <Divider style={{ margin: '1% 0 5% 0', width: '100%', minWidth: '30%' }} />
 
-        <TextField
-          id="standard-textarea"
-        //   label="Subheading"
-          placeholder="Subheading"
-          multiline
-          variant="standard"
-        //   value={draft[1]}
-          onChange={(e) => setSubHeading(e.target.value)}
-          style={{width:'100%',marginBottom: '28px' }}
-        />
+          <TextField
+            id="standard-textarea"
+            //   label=
+            placeholder="Heading"
+            multiline
+            variant="standard"
+            //   value={draft[0]} 
+            onChange={(e) => setHeading(e.target.value)}
+            style={{ width: '100%', marginBottom: '28px' }}
+          />
 
-        {/* {fieldComponent} */}
-        {para.map((object, index) => {
-            return(
-            <>
+          <TextField
+            id="standard-textarea"
+            //   label="Subheading"
+            placeholder="Subheading"
+            multiline
+            variant="standard"
+            //   value={draft[1]}
+            onChange={(e) => setSubHeading(e.target.value)}
+            style={{ width: '100%', marginBottom: '28px' }}
+          />
+
+          {/* {fieldComponent} */}
+          {para.map((object, index) => {
+            return (
+              <>
                 <TextField
-                id="standard-textarea"
-                placeholder="Write here"
-                multiline
-                variant="standard"
-                // value={object}
-                onChange={(e) => {handleChange(e,index)}}
-                style={{width:'90%',marginBottom: '28px',marginRight:'15px' }}
+                  id="standard-textarea"
+                  placeholder="Write here"
+                  multiline
+                  variant="standard"
+                  // value={object}
+                  onChange={(e) => { handleChange(e, index) }}
+                  style={{ width: '90%', marginBottom: '28px', marginRight: '15px' }}
                 />
-                <Button type="default" shape="circle" size="large" 
-                onClick={(e) => handleFieldChange(e,index) }>+</Button> 
-            </>
+                <Button shape="circle" size="large"
+                  onClick={(e) => handleFieldChange(e, index)}>+</Button>
+              </>
             );
-        })}
+          })}
 
-    {/* </Space> */}
-    </Col>
-    </Row>
+          {/* </Space> */}
+        </Col>
+      </Row>
     </>
-    );
+  );
 
 }
