860bd6b51c22a3b8f2293fcc7e62680680d0a32c
Mukhar Jain <Mukhar@users.noreply.github.com>
2021-11-18 23:12:53 +0530

    Update Controller.java

--- backend-bz/src/main/java/com/buzzpress/controller/Controller.java
+++ backend-bz/src/main/java/com/buzzpress/controller/Controller.java
@@ -2,17 +2,16 @@ package com.buzzpress.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import com.buzzpress.beans.UserStats;
 import com.buzzpress.beans.Users_;
 import com.buzzpress.dao.UserStatsRepository;
 import com.buzzpress.exception.DuplicateUserException;
 import com.buzzpress.model.FollowBody;
-import com.buzzpress.model.FollowBodyWrapper;
 import com.buzzpress.model.ResponseMessage;
 import com.buzzpress.service.IUserService;
-import com.buzzpress.service.impl.UserStatServiceImpl;
 
-import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,13 +19,12 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javassist.NotFoundException;
 
@@ -61,7 +59,7 @@ public class Controller {
     @PostMapping(value = "/saveusers")
     public ResponseEntity<ResponseMessage> postMethodName(@RequestBody List<Users_> entity)
             throws DuplicateUserException {
-        System.out.println(entity);
+        // System.out.println(entity);
         ResponseMessage rm = new ResponseMessage();
         rm.setMessage("Data Added");
         rm.setStatusCode(200);
